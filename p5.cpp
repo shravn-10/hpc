0f8de31645177da760d644d1e8a5250ff49648fd
Sai Shravan M <109055682+shravn-10@users.noreply.github.com>
2025-01-19 11:58:39 +0530

    Create p5.cpp

---
+++ p5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <omp.h>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, max_index = 0;
+    double max_cgpa = 0.0;
+
+    cout << "Enter the number of students: ";
+    cin >> n;
+
+    vector<double> cgpa(n);
+
+    cout << "Enter the CGPAs of the students:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Student " << i + 1 << ": ";
+        cin >> cgpa[i];
+    }
+
+    clock_t st = clock();
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        #pragma omp critical
+        {
+            if (cgpa[i] > max_cgpa) {
+                max_cgpa = cgpa[i];
+                max_index = i;
+            }
+        }
+    }
+
+    clock_t et = clock();
+    double time_taken = double(et - st) * 1000.0 / CLOCKS_PER_SEC;
+
+    cout << "Time Taken: " << time_taken << " ms" << endl;
+    cout << "The student with the highest CGPA is Student " << max_index + 1 << " with a CGPA of " << max_cgpa << endl;
+
+    return 0;
+}
