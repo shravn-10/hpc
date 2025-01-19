6d54d9ed598c28c295a66b76c8cb2d7342b4a542
Sai Shravan M <109055682+shravn-10@users.noreply.github.com>
2025-01-19 12:08:42 +0530

    Create p6.cpp

---
+++ p6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int rank, numproc;
+    int sum = 0;
+    int total_sum = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    srand(rank); 
+    sum = rand() % 100;
+
+    cout << "Robot " << rank << " picked " << sum << " mangoes." << endl;
+
+    MPI_Reduce(&sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        cout << "Total Mangoes picked by " << numproc << " Robots = " << total_sum << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
